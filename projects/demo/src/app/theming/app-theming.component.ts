c982f9da6611ef90ae189830fa94d95c9f85222f
Markus Windhager <44254831+rb-mwindh@users.noreply.github.com>
2023-02-03 09:07:09 +0100

    feat(app): initial implementation

---
+++ projects/demo/src/app/theming/app-theming.component.ts
@@ -0,0 +1,9 @@
+import { Component, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-theming',
+  template: '',
+  styleUrls: ['./theme-a.scss', './theme-b.scss', './theme-c.scss'],
+  encapsulation: ViewEncapsulation.None,
+})
+export class AppThemingComponent {}
