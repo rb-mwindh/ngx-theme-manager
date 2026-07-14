67c64225d9630baa62960e6eb753918105794366
Markus Windhager <44254831+rb-mwindh@users.noreply.github.com>
2026-07-14 10:09:48 +0200

    test(jest): align CLI and IDE test execution
    - explicitly configure the TypeScript project for Angular Jest targets
    - make Jest path resolution independent of the current working directory
    - support library subpath imports through Jest module mapping
    - centralize shared coverage configuration

--- jest.config.js
+++ jest.config.js
@@ -1,14 +1,25 @@
 module.exports = {
+  rootDir: __dirname,
   preset: 'jest-preset-angular',
-  moduleNameMapper: {
-    '@rb-mwindh/ngx-theme-manager':
-      '<rootDir>/projects/ngx-theme-manager/src/public-api.ts',
-  },
   modulePathIgnorePatterns: [
     '<rootDir>/node_modules/',
     '<rootDir>/dist/',
     '<rootDir>/package.json',
   ],
+  moduleNameMapper: {
+    '@rb-mwindh/ngx-theme-manager$':
+      '<rootDir>/projects/ngx-theme-manager/src/public-api.ts',
+    '@rb-mwindh/ngx-theme-manager/(.*)$':
+      '<rootDir>/projects/ngx-theme-manager/src/lib/$1',
+  },
   collectCoverage: true,
   coverageReporters: ['lcovonly', 'text'],
+  // coverageThreshold: {
+  //   global: {
+  //     branches: 100,
+  //     functions: 100,
+  //     lines: 100,
+  //     statements: 100,
+  //   },
+  // },
 };
--- projects/demo/jest.config.js
+++ projects/demo/jest.config.js
@@ -1,6 +1,5 @@
 module.exports = {
   coverageDirectory: '<rootDir>/coverage/demo',
-  coveragePathIgnorePatterns: ['/ngx-theme-manager/', '/node_modules/'],
   collectCoverageFrom: [
     '<rootDir>/projects/demo/src/app/**/*.ts',
     '!**/index.ts',
